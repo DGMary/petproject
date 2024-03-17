1ef7d1e575b92fa69b3e99589f66101df2c162dc
mariia.mynkina <mariia.mynkina@danads.se>
2024-03-17 11:30:58 +0200

    Test #2

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,20 @@ import "./App.css";
 
 function App() {
   let [count, setCount] = useState(0);
-  const test =0;
-
   return (
     <>
-         <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+
+
+
+
+              <div>
+                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+                  <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank" rel="noreferrer">
+                  <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+              </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
